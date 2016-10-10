f601382b41e63cc82e8f73523e378e3e00e83d88
James Gay <jamesrkg@gmail.com>
2016-10-10 13:36:42 +0100

    added error message for incomplete input

--- diceroll.py
+++ diceroll.py
@@ -1,10 +1,17 @@
+## diceroll simulator
+## 10/10/16 -jrkg
+
 import random
 import sys
+if len(sys.argv) == 0:
+    print("Please specify the number of dice to roll.")
+    sys.ext(1)
 
 number_of_dice = int(sys.argv[1])
 min = 1
 max = 6
 
+print("The dice values are")
 while number_of_dice >0:
     roll = random.randint(min,max)
     print(roll)
