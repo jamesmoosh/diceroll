da9bd7b922f4f7c80da985bf759c1d899fe82909
James Gay <jamesrkg@gmail.com>
2016-10-10 13:31:57 +0100

    improved for multiple dice

--- diceroll.py
+++ diceroll.py
@@ -1,7 +1,13 @@
 import random
+import sys
+
+number_of_dice = int(sys.argv[1])
 min = 1
 max = 6
 
-roll = random.randint(min,max)
+while number_of_dice >0:
+    roll = random.randint(min,max)
+    print(roll)
+    number_of_dice = number_of_dice - 1
 
-print(roll)
+    
